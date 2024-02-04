1777cc02f036748c75e3ba3051014350ad6c5ea6
farozy <farozy12@gmail.com>
2024-02-04 17:13:02 +0700

    update api contact

--- src/config/API.ts
+++ src/config/API.ts
@@ -1,2 +1,2 @@
 // export const API_CONTACT = 'http://localhost:3000/api/contacts';
-export const API_CONTACT = 'https://wedding-invitation-farozy-dan-ukhtye-9o7lzbt57.vercel.app/api/contacts';
+export const API_CONTACT = 'https://wedding-invitation-farozy-dan-ukhty.vercel.app/api/contacts';
