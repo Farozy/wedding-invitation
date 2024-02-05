d2935bf5cf1c343e88e22e017b51e29c84f0cc6c
farozy <farozy12@gmail.com>
2024-02-05 20:18:56 +0700

    update app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,42 +1,48 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import type {Metadata} from 'next'
+import {Inter} from 'next/font/google'
 import './globals.css'
 import React from "react";
 import {GlobalContextProvider} from "@/app/context/store";
+import Head from 'next/head';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({subsets: ['latin']})
 
 export const metadata: Metadata = {
-  title: 'Undangan Pernikahan',
-  description: 'undangan elektronik untuk pernikahan',
-  icons: {
-    icon: [
-      {
-        media: '(prefers-color-scheme: light)',
-        url: '/faviconn.png',
-        href: '/faviconn.png',
-      },
-      {
-        media: '(prefers-color-scheme: dark)',
-        url: '/faviconn.png',
-        href: '/faviconn.png',
-      },
-    ]
-  }
+    title: 'Undangan Pernikahan',
+    description: 'undangan elektronik untuk pernikahan',
+    icons: {
+        icon: [
+            {
+                media: '(prefers-color-scheme: light)',
+                url: '/faviconn.png',
+                href: '/faviconn.png',
+            },
+            {
+                media: '(prefers-color-scheme: dark)',
+                url: '/faviconn.png',
+                href: '/faviconn.png',
+            },
+        ]
+    }
 }
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
+export default function RootLayout({children}: {
+    children: React.ReactNode
 }) {
-  return (
-    <html lang="en">
-      <body className={inter.className} suppressHydrationWarning={true}>
+    return (
+        <html lang="en">
+        <Head>
+            <title>My page title</title>
+            <meta property="og:title" content="My page title" key="title"/>
+        </Head>
+        <Head>
+            <meta property="og:title" content="My new title" key="title"/>
+        </Head>
+        <body className={inter.className} suppressHydrationWarning={true}>
         <GlobalContextProvider>
-          {children}
+            {children}
         </GlobalContextProvider>
-      </body>
-    </html>
-  )
+        </body>
+        </html>
+    )
 }
